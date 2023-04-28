ee7b378be9a4221b3b218f83086d6e2a689ef03b
Zaire Ali <zaireali649@gmail.com>
2023-04-28 00:42:29 +0000

    useful commands

---
+++ useful_commands.py
@@ -0,0 +1,21 @@
+import random
+
+var = 2
+
+print(type(var))
+
+var2 = "str"
+
+print(type(var2))
+
+print(dir(var2))
+
+print(var2.capitalize())
+
+var3 = "9"
+
+print(var3.zfill(3))
+
+number = random.randint(0,100)
+
+print(number)
