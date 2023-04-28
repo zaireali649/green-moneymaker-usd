37a6267ad27a332434262b13d7a274089e5e9383
Zaire Ali <zaireali649@gmail.com>
2023-04-28 00:44:21 +0000

    inline comment code

--- hello_name.py
+++ hello_name.py
@@ -1,4 +1,7 @@
+# This line prints the name of the current module to the console
 print(__name__)
 
+# This line checks if the current file is being run as the main program
 if __name__ == '__main__':
+    # If it is being run as the main program, this line prints "Hello World" to the console
     print("Hello World")
--- test_name.py
+++ test_name.py
@@ -1,3 +1,5 @@
+# This line imports all functions from the "hello_name" module
 from hello_name import *
 
+# This line prints the name of the current module to the console
 print(__name__)
