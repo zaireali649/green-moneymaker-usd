0e1dec83f949002a0d5c3b5362e2c3af8b08897b
Zaire Ali <zaireali649@gmail.com>
2023-05-05 00:56:24 +0000

    use module

---
+++ use_module.py
@@ -0,0 +1,6 @@
+import functions
+
+a = 2  # Assign the value 2 to variable a
+b = 3  # Assign the value 3 to variable b
+c = functions.sum(a, b)  # Call the sum function from the functions module and assign the result to variable c
+print(a, b, c)  # Print the values of a, b, and c to the console
